3a04b7d4012aacd3063d93ca96ac17749b558056
C. M. McColeman <cmccolem@sfu.ca>
2018-03-03 22:49:55 -0600

    added the reproducibility info

--- ratioFits/saveTrialData_barGraphType.m
+++ ratioFits/saveTrialData_barGraphType.m
@@ -7,10 +7,13 @@ function saveTrialData_barGraphType(subID, ...  participantID
     sameOrDiffTrial, ... sameOrDiffTrial
     recordedAnswer, ... letterResponse
     trialAcc, ... accuracy
+    ratioArrayOpts, ... on "same" trials, ratioArrayOpts(ratioArrayIdx) is the presented value
     ratioArrayIdx, ... % whichRatio
     questObject, ... % for the SA (staircase analysis) variables
-    presentedRatio... % presentedRatio; stimulus value in ratio space
-    )
+    presentedRatio,... % presentedRatio; stimulus value in ratio space
+    stimRect, ... % [x1 y1 x2 y2]; the rectangle values for the manipulated stim
+    refRect,... % [x1 y1 x2 y2]; the rectangle values for the reference stim
+    presentationOrder)
 
 %
 %  Author: Caitlyn McColeman
@@ -41,6 +44,7 @@ function saveTrialData_barGraphType(subID, ...  participantID
 %
 %
 %       % STAIRCASE ANALYSIS VARIABLES % save only for "different" trials
+%
 %              testedRatio, decimal; qu.referenceRatio
 %       ratioPresentations, integer; qu.trialCount
 %        abstractIntensity, decimal; qu.intensity(qu.trialCount)
@@ -49,7 +53,14 @@ function saveTrialData_barGraphType(subID, ...  participantID
 %                 response, logical; qu.response
 %            quantileOrder, decimal; qu.quantileOrder
 %
-%      % formatting text file %
+%       % DETAIL METHOD VARIABLES % for debugging and re-drawing stimuli as
+%                                   needed
+%
+%         stimRectLeft, string; mat2str(stimRect(:,1))
+%        stimRectRight, string; mat2str(stimRect(:,2))
+%          refRectLeft, string; mat2str(refRect(:,1))
+%         refRectRight, string; mat2str(refRect(:,2))
+%    presentationOrder, string; mat2str(presentationOrder)
 
 
 
@@ -61,38 +72,20 @@ dataIn_BA   = { num2str(subID)     stimType     trialIterator  sameOrDiffTrial  
 
 
 % staircase analysis
-varNames_SA   = { 'testedRatio',       'nRatioPresentations',     'abstractIntensity',       'presentedRatio',          'estimatedThreshold',          'response',          'quantileOrder'};
+varNames_SA   = { 'testedRatio',       'nRatioPresentations',     'abstractIntensity',       'presentedRatio',       'estimatedThreshold',     'response',          'quantileOrder',        'TTest'};
 if strcmpi(sameOrDiffTrial, 'different')
     qu = questObject;
-    dataIn_SA ={mat2str(qu.referenceRatio)    qu.trialCount        qu.intensity(qu.trialCount)  mat2str(presentedRatio)    qu.xThreshold     qu.response(qu.trialCount)   qu.quantileOrder  };
+    tTest = QuestQuantile(qu);
+    dataIn_SA ={mat2str(qu.referenceRatio)        qu.trialCount    qu.intensity(qu.trialCount)  mat2str(presentedRatio)    qu.xThreshold     qu.response(qu.trialCount)   qu.quantileOrder       tTest };
 else % qu object will not be updated on 'same' trials. Just save some blank values to hold place
-    dataIn_SA ={mat2str(presentedRatio)          NaN                         NaN                mat2str(presentedRatio)            NaN                     trialAcc             NaN     };
+    dataIn_SA ={mat2str(ratioArrayOpts(ratioArrayIdx,:)) NaN                NaN     mat2str(ratioArrayOpts(ratioArrayIdx,:))    NaN                 trialAcc             NaN                     NaN  };
 end
-varTypes_SA   = [    '%s\t                  %d\t                     %3.6f\t                    %s\t                       %3.6f\t                     %u\t                    %1.6f '  ];
+varTypes_SA   = [    '%s\t                            %d\t              %3.6f\t                    %s\t                    %3.6f\t                %u\t                %1.6f\t                %1.6f\t  '  ];
 
-
-if ~(exist([num2str(subID) 'trialLvl.txt'])==2)
-    fID = fopen([ num2str(subID) 'trialLvl.txt'], 'a+'); % open file
-    
-    % all the variable names are strings; save as such
-    varTypes_names = repmat('%s\t ', 1, length(varNames_BA)+length(varNames_SA));
-    
-    namesIn = {varNames_BA{:} varNames_SA{:}};
-    
-    % push to file
-    fprintf(fID, [varTypes_names '\n'], namesIn{:}); % save data
-    
-    % close connection to file
-    fclose(fID)
-end
-
-
-% Open/create a file named after this subject; spec. permission to append
-fID = fopen([ num2str(subID) 'trialLvl.txt'], 'a+');
-
-dataIn = {dataIn_BA{:} dataIn_SA{:}};
-fprintf(fID, [varTypes_BA varTypes_SA  '\n'], dataIn{:}); % save data
-fclose(fID); % close the file connection
+% reproducible trial DEtails
+varNames_DE   = { 'StimLeftBarLeft', 'StimLeftBarTop', 'StimLeftBarRight', 'StimLeftBarBottom', 'StimRightBarTop', 'StimRightBarLeft', 'StimRightBarBottom', 'StimRightBarRight', 'refStimOrder'} ;
+  dataIn_DE   = {                                                               stimRect(:,1)'                                                                    stimRect(:,2)'   mat2str(presentationOrder)};
+varTypes_DE   = [    ' %4.4f\t             %4.4f\t             %4.4f\t          %4.4f\t             %4.4f\t             %4.4f\t             %4.4f\t                 %4.4f\t         %s'  ];
 
 %
 %
@@ -112,5 +105,28 @@ fclose(fID); % close the file connection
 %  Additional Comments:
 
 
+% Create header row
+if ~(exist([num2str(subID) 'trialLvl.txt'])==2)
+    fID = fopen([ num2str(subID) 'trialLvl.txt'], 'a+'); % open file
+    
+    % all the variable names are strings; save as such
+    varTypes_names = repmat('%s\t ', 1, length(varNames_BA)+length(varNames_SA)+length(varNames_DE));
+    
+    namesIn = {varNames_BA{:} varNames_SA{:} varNames_DE{:}};
+    
+    % push to file
+    fprintf(fID, [varTypes_names '\n'], namesIn{:}); % save data
+    
+    % close connection to file
+    fclose(fID)
+end
+
+
+% Open/create a file named after this subject; spec. permission to append
+fID = fopen([ num2str(subID) 'trialLvl.txt'], 'a+');
+
+dataIn = {dataIn_BA{:} dataIn_SA{:} dataIn_DE{:}};
+fprintf(fID, [varTypes_BA varTypes_SA varTypes_DE '\n'], dataIn{:}); % save data
+fclose(fID); % close the file connection
 
 
