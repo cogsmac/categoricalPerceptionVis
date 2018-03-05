ac0c8dddda7ac59d1b30350e4a4b1c9f55b07aa5
C. M. McColeman <cmccolem@sfu.ca>
2018-03-04 18:04:52 -0600

    single bar organization

--- ratioFits/barGraphType.m
+++ ratioFits/barGraphType.m
@@ -4,7 +4,7 @@
 %  Date Created: February 27 2018
 %  Last Edit: [Last Time of Edit]
 %
-function [rectangleData heightData]=barGraphType(ratioValues, position, screenDimensions)
+function [rectangleData, heightData]=barGraphType(ratioValues, position, screenDimensions, stimType, referenceBarIdx)
 %
 %  Visual Thinking Lab, Northwestern University
 %  Originally Created For: ratio1
@@ -23,7 +23,7 @@ function [rectangleData heightData]=barGraphType(ratioValues, position, screenDi
 
 % call function to get stimulus size, position, properties of
 % the changing, thresholded stimlus
-[rectangleData1, rectangleData2, height1, height2] = ratio1StimulusVals(ratioValues, {'position', position}, screenDimensions);
+[rectangleData1, rectangleData2, height1, height2] = ratio1StimulusVals(ratioValues, {'position', position}, screenDimensions, position, stimType, referenceBarIdx);
 
 rectangleData = [rectangleData1', rectangleData2'];
 heightData = [height1 height2];
