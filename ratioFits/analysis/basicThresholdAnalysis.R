2324536a2348b7818ae68a817de086d0f523a285
C. M. McColeman <cmccolem@sfu.ca>
2018-04-11 13:55:21 -0500

    working for ratio2

--- ratioFits/analysis/basicThresholdAnalysis.R
+++ ratioFits/analysis/basicThresholdAnalysis.R
@@ -1,6 +1,6 @@
 #  Author: Caitlyn McColeman
 #  Date Created: March 5 2018
-#  Last Edit: 
+#  Last Edit: Apr 11; aesthetic updates
 #
 #  Visual Thinking Lab, Northwestern University
 #  Originally Created For: ratio1
@@ -13,6 +13,7 @@
 #  OUTPUT: 
 #
 #  Additional Comments:
+#     updated Mar 22 to work with ratio2 instead of ratio1
 #
 #  Additional Scripts Used:
 
@@ -21,18 +22,25 @@ library(ggplot2)
 library(plyr)
 library(ez)
 
+expName = 'ratio2'
+wdName = '../ratioFits_data2'
+
 currDir = dirname(sys.frame(1)$ofile) # remember where we came from so we can save plots to the appropriate spot
-setwd('../ratioFits_Data') 
+setwd(wdName) 
 
 # get names of files
 all.files <- list.files(pattern = "trialLvl.txt")
 
 # load data, organize it into meaningful columns
-fullDataSet <- lapply(all.files, read.table, sep="\t", header = TRUE, fill = TRUE)
-dTableShaped <- rbindlist(fullDataSet)
+fullDataSet <- lapply(all.files, read.table, sep="\t", header = TRUE, fill = T, fileEncoding="US-ASCII")
+dTableShaped <- rbindlist(fullDataSet, 'fill' = T)
 
 # the automatic order of the testedRatio doesn't make much sense. Reorder them so comparison is easier. 
-dTableShaped$testedRatio = factor(dTableShaped$testedRatio,levels(dTableShaped$testedRatio)[c(1,4,2,5,3,6)])
+dTableShaped$testedRatio = factor(dTableShaped$testedRatio,levels(dTableShaped$testedRatio)[c(1,7,14,6,
+                                                                                              3,9,2,8,
+                                                                                              4,19,15,10,
+                                                                                              5,12,16,11,
+                                                                                              18,13,17,20)])
 
 setkey(dTableShaped , participantID, trialID)
 dTableShaped[ is.na(dTableShaped) ] <- NA # replace MATLAB NaN with R-friendly NA 
@@ -42,16 +50,18 @@ dTableShaped$participantID<-as.factor(dTableShaped$participantID)
 tasksIn = unique(dTableShaped$comparisonTask)
 barGraph = dTableShaped[dTableShaped$comparisonTask == tasksIn[1],]
 barOnly = dTableShaped[dTableShaped$comparisonTask == tasksIn[2],]
+stkOnly = dTableShaped[dTableShaped$comparisonTask == tasksIn[3],]
 
 ## visualize the development of the final estimated values by trial 
 bgOut = ggplot(barGraph, aes(x=trialID, y=estimatedThreshold, colour=participantID)) + 
   geom_errorbar(aes(ymin=estimatedThreshold-estThresholdSD, ymax=estimatedThreshold+estThresholdSD), width=.1, alpha = .3) +
   geom_point(size = .5, alpha = .5) + 
-  facet_wrap(~testedRatio, nrow = 6) +
+  facet_wrap(~testedRatio, nrow = 5) +
   theme_light() + 
   theme(panel.grid.minor = element_blank(),
-        strip.background = element_rect(colour="grey", fill="#E0E0E0"),
-        panel.border = element_rect(colour = "black")) # leave facet titles for my pre-decision; make another version and label w graphs
+        strip.background = element_rect(colour="black", fill="#E0E0E0"),
+        panel.border = element_rect(colour = "black"))+
+        theme(strip.text = element_text(colour = 'black')) # leave facet titles for my pre-decision; make another version and label w graphs
 
 
 barOnlyOut = ggplot(barOnly, aes(x=trialID, y=estimatedThreshold, colour=participantID)) + 
@@ -60,7 +70,16 @@ barOnlyOut = ggplot(barOnly, aes(x=trialID, y=estimatedThreshold, colour=partici
   facet_wrap(~testedRatio, nrow = 6) +
   theme_light() + 
   theme(panel.grid.minor = element_blank(),
-        strip.background = element_rect(colour="grey", fill="#E0E0E0"),
+        strip.background = element_rect(colour="grey", fill="#606060"),
+        panel.border = element_rect(colour = "black")) # leave facet titles for my pre-decision; make another version and label w graphs
+
+stkOnlyOut = ggplot(stkOnly, aes(x=trialID, y=estimatedThreshold, colour=participantID)) + 
+  geom_errorbar(aes(ymin=estimatedThreshold-estThresholdSD, ymax=estimatedThreshold+estThresholdSD), width=.1, alpha = .3) +
+  geom_point(size = .5, alpha = .5) + 
+  facet_wrap(~testedRatio, nrow = 6) +
+  theme_light() + 
+  theme(panel.grid.minor = element_blank(),
+        strip.background = element_rect(colour="grey", fill="#606060"),
         panel.border = element_rect(colour = "black")) # leave facet titles for my pre-decision; make another version and label w graphs
 
 # this user function will take the subset of data that are of interest ('barGraph' type or 'barOnly' type) and spit out an ANOVA and a couple of graphs
@@ -148,11 +167,11 @@ conditionsIn  = unique(fullSet$btwnSubCond)
 
 # plot the data summary
 ratio1Pilot = ggplot() + 
-   geom_point(data = summaryRatio1[summaryRatio1$stimulus== conditionsIn[1],], aes(x=testedRatio, y = mean), size = 5, alpha = .5, colour = "blue") + 
-   geom_point(data = summaryRatio1[summaryRatio1$stimulus== conditionsIn[2],], aes(x=testedRatio, y = mean), size = 5, alpha = .5, colour = "red") + 
+   geom_point(data = summaryRatio1[summaryRatio1$stimulus== conditionsIn[1],], aes(x=testedRatio, y = mean), size = 5, alpha = .5, colour = "purple") + 
+   geom_point(data = summaryRatio1[summaryRatio1$stimulus== conditionsIn[2],], aes(x=testedRatio, y = mean), size = 5, alpha = .5, colour = "#228B22") + 
    #geom_errorbar(data = summaryRatio1, aes(x=testedRatio, ymin=mean-SD, ymax=mean+SD), width=1, alpha = .3) +
-   geom_point(data = fullSet[fullSet$btwnSubCond == conditionsIn[1],], aes(x=testedRatio, y = exp(estimatedThreshold)), size = 2, alpha = .1, colour = "blue") +
-   geom_point(data = fullSet[fullSet$btwnSubCond == conditionsIn[2],], aes(x=testedRatio, y = exp(estimatedThreshold)), size = 2, alpha = .1, colour = "red") +
+   geom_point(data = fullSet[fullSet$btwnSubCond == conditionsIn[1],], aes(x=testedRatio, y = exp(estimatedThreshold)), size = 2, alpha = .1, colour = "purple") +
+   geom_point(data = fullSet[fullSet$btwnSubCond == conditionsIn[2],], aes(x=testedRatio, y = exp(estimatedThreshold)), size = 2, alpha = .1, colour = "#228B22") +
    facet_wrap(~stimulus, nrow = 1) +
    theme_light() + 
    theme(panel.grid.minor = element_blank(),
